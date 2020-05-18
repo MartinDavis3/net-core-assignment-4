e6938f638c5d06c358b69d84935a327b8b3f6579
Martin Davis <martin_davis3@yahoo.ca>
2020-05-18 11:07:24 -0600

    add models

---
+++ AspNetCoreStore/Models/StoreItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreStore.Models
+{
+    public class StoreItem
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
---
+++ AspNetCoreStore/Models/StoreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreStore.Models
+{
+    public class StoreViewModel
+    {
+        public StoreItem[] Items { get; set; }
+    }
+}
